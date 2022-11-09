6217e3cb41e76a73f89549cc85a9c64c3abceff9
Lizzie <99427499+lizzieeli@users.noreply.github.com>
2022-11-09 15:21:39 -0600

    Update Cat_Rat_Zodiac_Code.ino
    first interaction done

--- Cat_Rat_Zodiac_Code/Cat_Rat_Zodiac_Code.ino
+++ Cat_Rat_Zodiac_Code/Cat_Rat_Zodiac_Code.ino
@@ -1,13 +1,55 @@
+#include <Servo.h>
+
+Servo arrow;
+
+const int ledOnePin = 11;
+const int ledTwoPin = 10;
+const int ledThreePin = 9;
+
+const int buttonOnePin = 2;
+int buttonOneState = 0;
+int lastButtonOneState = 0;
+
+const int buttonTwoPin = 3;
+
+bool stageTwo = false;
+bool stageThree = false;
+
 void setup() {
-  // put your setup code here, to run once:
-  // word this is me editing code for my own branch
-  // this is lizzie testing
-  // yippeeeee
+pinMode(ledOnePin, OUTPUT);
+pinMode(ledTwoPin, OUTPUT);
+pinMode(ledThreePin, OUTPUT);
+
+pinMode(buttonOnePin, INPUT);
+pinMode(buttonTwoPin, INPUT);
+pinMode(buttonThreePin, INPUT);
+pinMode(buttonFourPin, INPUT);
+pinMode(buttonFivePin, INPUT);
+
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // evelyn test for evelyn branch
-  
+
+//interaction one
+if (!stageTwo) {
+  //led one is on by default
+  digitalWrite(ledOnePin, HIGH);
+  buttonOneState = digitalRead(buttonOnePin);
+
+  if(buttonOneState != lastButtonOneState) {
+    if (buttonOneState == HIGH) {
+      stageTwo = true;
+    }
+  }
+} if (stageTwo == true) {
+  //button one activates led one to turn off
+  {
+    digitalWrite(ledOnePin, LOW);
+    digitalWrite(ledTwoPin, HIGH);
+  }
+}
+
+//interaction two
 
 }
